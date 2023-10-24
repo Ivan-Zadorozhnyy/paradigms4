804894f91856aaf74c0a644d0cc0fe93c4aae021
Ivan <zadorohhnyiv@gmail.com>
2023-10-24 22:07:32 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
-using namespace std;
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <filesystem>
+
+extern "C" {
+char* encrypt(char* rawText, int key);
+char* decrypt(char* encryptedText, int key);
+}
 
 class IReader
 {
@@ -46,3 +54,73 @@ public:
         file.close();
     }
 };
+
+int main() {
+    int choice, key, mode;
+    std::string inputPath, outputPath;
+    char* buffer;
+    std::streamsize size;
+
+    IReader* reader = new FileReader();
+    IWriter* writer = new FileWriter();
+
+    std::cout << "Choose an operating mode:" << std::endl;
+    std::cout << "1. Normal mode" << std::endl;
+    std::cout << "2. Secret mode" << std::endl;
+    std::cout << "Enter mode choice: ";
+    std::cin >> mode;
+    std::cin.ignore();
+
+    if (mode == 1) {
+        std::cout << "Choose an option:" << std::endl;
+        std::cout << "1. Encrypt a file" << std::endl;
+        std::cout << "2. Decrypt a file" << std::endl;
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+        std::cin.ignore();
+
+        std::cout << "Enter the input file path: ";
+        std::getline(std::cin, inputPath);
+
+        reader->read(inputPath, buffer, size);
+
+        std::cout << "Enter the output file path: ";
+        std::getline(std::cin, outputPath);
+
+        std::cout << "Enter the key for operation: ";
+        std::cin >> key;
+
+        if (choice == 1) {
+            char* encrypted = encrypt(buffer, key);
+            writer->write(outputPath, encrypted, size);
+            delete[] encrypted;
+        } else if (choice == 2) {
+            char* decrypted = decrypt(buffer, key);
+            writer->write(outputPath, decrypted, size);
+            delete[] decrypted;
+        }
+
+    } else if (mode == 2) {
+        std::cout << "Secret mode activated." << std::endl;
+        std::cout << "Enter the path of the file to encrypt: ";
+        std::getline(std::cin, inputPath);
+
+        reader->read(inputPath, buffer, size);
+
+        key = generateRandomKey();
+        std::cout << "Using generated encryption key: " << key << std::endl;
+        char* encrypted = encrypt(buffer, key);
+
+        std::cout << "Enter the path to save the encrypted file: ";
+        std::getline(std::cin, outputPath);
+        writer->write(outputPath, encrypted, size);
+
+        delete[] encrypted;
+    }
+
+    delete[] buffer;
+    delete reader;
+    delete writer;
+
+    return 0;
+}
